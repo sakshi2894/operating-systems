2263139abaaa0ca2c356a445ff5e8bbaac6389f7
Sakshi Bansal <sakshi@rockhopper-09.cs.wisc.edu>
2020-02-12 16:42:04 -0600

    reformatted and added other calls

--- lab2a/smash.c
+++ lab2a/smash.c
@@ -8,12 +8,177 @@
 #include <zconf.h>
 
 
-
 typedef struct Node {
     char* data;
     struct Node *next;
 } Node;
 
+
+void handle_error();
+Node* init_path();
+void set_cmd_args(char* cmd, int* cmd_size, char* cmd_args_arr[]);
+void handle_path(char* cmd_args_arr[], int cmd_size, Node *path_head);
+void addToFront(Node **head, char *data);
+void removeNode(Node **head, char *data);
+void clear(Node **head);
+void printList(Node *head);
+int get_size(Node *head);
+
+
+int main(int argc, char** argv) {
+
+    FILE *f;
+    int is_batch = 0;
+    if (argc < 1) {
+        handle_error();
+        exit(1);
+    } else if (argc == 1){
+        f = stdin;
+    } else {    //argc == 2
+        char *filename = argv[1];
+        FILE *f = fopen(filename, "r");
+        if (f == NULL) {
+            handle_error();
+            exit(1);
+        }
+        is_batch = 1;
+    }
+
+    if (is_batch == 0) {
+        printf("smash>");
+    }
+    char *line = NULL;
+    size_t len = 0;
+
+    Node *path_head = init_path();
+
+    while (getline(&line, &len, f) != -1) {
+
+        char* cmds = strsep(&line, ";");
+
+        while (cmds != NULL) {
+
+            char* cmd = strdup(cmds);
+
+            if (strlen(cmd) > 0) {
+
+                int cmd_size = 0;
+                char* cmd_args_arr[500];
+
+                set_cmd_args(cmd, &cmd_size, cmd_args_arr);
+
+                //Built-in commands.
+                if (strcmp(cmd_args_arr[0], "exit") == 0) {
+                    exit(0);
+                } else if (strcmp(cmd_args_arr[0], "cd") == 0) {
+                    if (cmd_size == 1 || cmd_size > 2 ) {
+                        handle_error();
+                    }
+                    chdir(cmd_args_arr[1]);
+                } else if (strcmp(cmd_args_arr[0], "path") == 0) {
+                    handle_path(cmd_args_arr, cmd_size, path_head);
+                } else {        //Other commands.
+                    // Look for commands in path directories.
+                    Node *pNode = path_head;
+                    int cmd_run = 0;
+                    while (pNode != NULL) {
+                        char* path_dir = pNode->data;
+                        char* cmd_in_arg = strdup(cmd_args_arr[0]);
+                        char * full_path = (char *) malloc(2 + strlen(path_dir)+ strlen(cmd_in_arg) );
+
+                        strcat(full_path, path_dir);
+                        strcat(full_path, "/");
+                        strcat(full_path, cmd_in_arg);
+                        printf("full path is: %s\n", full_path);
+
+                        if (access(full_path, X_OK) == 0) {
+                            cmd_run = 1;
+                            int rc = fork();
+                            if (rc == 0) {
+                                cmd_args_arr[0] = full_path;
+                                execv(full_path, cmd_args_arr);
+                                handle_error();
+                            } else {
+                                wait(NULL);
+                            }
+                            pNode = NULL;
+                        } else {
+                            pNode = pNode->next;
+                        }
+                    }
+                    if (cmd_run == 0) {
+                        handle_error();
+                    }
+                }
+
+            }
+
+            cmds = strsep(&line, ";");
+        }
+
+        if (is_batch == 0) {
+            printf("smash>");
+        }
+
+    }
+    free(line);
+
+}
+
+void handle_error() {
+    char error_message[30] = "An error has occurred\n";
+    write(STDERR_FILENO, error_message, strlen(error_message));
+}
+
+Node* init_path() {
+    Node *head = (Node*) malloc(sizeof(Node));
+    head -> data = "/bin";
+    head -> next = NULL;
+    return head;
+}
+
+void set_cmd_args(char* cmd, int* cmd_size, char* cmd_args_arr[]) {
+
+    char *cmd_args = strsep(&cmd, " ");
+    while (cmd_args != NULL) {
+        if (strlen(cmd_args) > 0) {
+            //Handle trailing \n
+            size_t ln = strlen(cmd_args) - 1;
+            if (cmd_args[ln] == '\n') cmd_args[ln] = '\0';
+
+            cmd_args_arr[*cmd_size] = cmd_args;
+            *cmd_size = *cmd_size + 1;
+        }
+        cmd_args = strsep(&cmd, " ");
+    }
+}
+
+
+void handle_path(char* cmd_args_arr[], int cmd_size, Node *path_head) {
+    if (cmd_size < 2) {
+        handle_error();
+    }
+    char* path_cmd = cmd_args_arr[1];
+    if (strcmp(path_cmd, "add") == 0) {
+        if (cmd_size != 3) handle_error();
+        addToFront(&path_head, cmd_args_arr[2]);
+        printList(path_head);
+    } else if (strcmp(path_cmd, "remove") == 0) {
+        if (cmd_size != 3) handle_error();
+        removeNode(&path_head, cmd_args_arr[2]);
+        printList(path_head);
+    } else if (strcmp(path_cmd, "clear") == 0) {
+        if (cmd_size != 2) handle_error();
+        clear(&path_head);
+        printList(path_head);
+    } else {
+        handle_error();
+    }
+}
+
+
+
+//Linked list methods below
 
 void addToFront(Node **head, char *data) {
 
@@ -77,131 +242,4 @@ int get_size(Node *head) {
         temp = temp -> next;
     }
     return size;
-}
-
-void handle_error();
-Node* init_path();
-
-int main(int argc, char** argv) {
-
-    FILE *f;
-    int is_batch = 0;
-    if (argc < 1) {
-        handle_error();
-    } else if (argc == 1){
-        f = stdin;
-    } else {    //argc == 2
-        char *filename = argv[1];
-        FILE *f = fopen(filename, "r");
-        if (f == NULL) {
-            handle_error();
-        }
-        is_batch = 1;
-    }
-
-    if (is_batch == 0) {
-        printf("smash>");
-    }
-    char *line = NULL;
-    size_t len = 0;
-
-    Node *path_head = init_path();
-
-    while (getline(&line, &len, f) != -1) {
-
-        char* cmds = strsep(&line, ";");
-
-        while (cmds != NULL) {
-
-            char* cmd = strdup(cmds);
-
-            if (strlen(cmd) > 0) {
-
-                char *cmd_args_arr[500];
-                char *cmd_args = strsep(&cmd, " ");
-                int cmd_size = 0;
-                while (cmd_args != NULL) {
-                    if (strlen(cmd_args) > 0) {
-                        //Handle trailing \n
-                        size_t ln = strlen(cmd_args) - 1;
-                        if (cmd_args[ln] == '\n') cmd_args[ln] = '\0';
-
-                        cmd_args_arr[cmd_size] = cmd_args;
-                        cmd_size = cmd_size + 1;
-                    }
-                    cmd_args = strsep(&cmd, " ");
-                }
-
-                if (strcmp(cmd_args_arr[0], "exit") == 0) {
-                    exit(0);
-                } else if (strcmp(cmd_args_arr[0], "cd") == 0) {
-                    if (cmd_size == 1 || cmd_size > 2 ) {
-                        handle_error();
-                    }
-                    chdir(cmd_args_arr[1]);
-                } else if (strcmp(cmd_args_arr[0], "path") == 0) {
-                    if (cmd_size < 2) {
-                        handle_error();
-                    }
-                    char* path_cmd = cmd_args_arr[1];
-                    if (strcmp(path_cmd, "add") == 0) {
-                        if (cmd_size != 3) handle_error();
-                        addToFront(&path_head, cmd_args_arr[2]);
-                        printList(path_head);
-                    } else if (strcmp(path_cmd, "remove") == 0) {
-                        if (cmd_size != 3) handle_error();
-                        removeNode(&path_head, cmd_args_arr[2]);
-                        printList(path_head);
-                    } else if (strcmp(path_cmd, "clear") == 0) {
-                        if (cmd_size != 2) handle_error();
-                        clear(&path_head);
-                        printList(path_head);
-                    } else {
-                        handle_error();
-                    }
-                   // printf("path printed");
-                } else {
-                    //printf("arg 0 is: %s", cmd_args_arr[0]);
-                }
-
-                /**
-                char *cmd_args[len];
-                //printf("str is at %p p is set at %p\n", str, p);
-
-                while (p != NULL) {
-                    if (strlen(p) > 0) {
-                        printf("%s\n", p);
-                    }
-
-                    //Knows when to resume.
-                    // Chnages str to store this context information.
-                    // That's why doesn't work on constant strings.
-                    //valgrind ./
-                    p = strsep(&line, " ");
-                }**/
-            }
-
-            cmds = strsep(&line, ";");
-        }
-
-        if (is_batch == 0) {
-            printf("smash>");
-        }
-
-    }
-    free(line);
-
-}
-
-void handle_error() {
-    char error_message[30] = "An error has occurred\n";
-    write(STDERR_FILENO, error_message, strlen(error_message));
-    exit(1);
-}
-
-Node* init_path() {
-    Node *head = (Node*) malloc(sizeof(Node));
-    head -> data = "/bin";
-    head -> next = NULL;
-    return head;
 }
